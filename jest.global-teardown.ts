751c2911b2967154c95f84464aa3db8fcb07d468
Vikas Surera <vikassurera@gmail.com>
2022-06-13 17:51:41 +0530

    added global teardown

--- jest.global-teardown.ts
+++ jest.global-teardown.ts
@@ -1,5 +1,7 @@
-module.exports = function globalTeardown(done) {
+const { teardown: teardownPuppeteer } = require('jest-environment-puppeteer');
+module.exports = function globalTeardown() {
     // do stuff which needs to be done before all tests are executed
     console.log('globalTeardown');
+    teardownPuppeteer();
     return;
 };
